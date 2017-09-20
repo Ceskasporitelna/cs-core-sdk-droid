746754b081f1590b583f65c8fb3eee6d3c006004
Jan Hauser <hauseja3@gmail.com>
2017-09-20 11:14:26 +0200

    add failure migration scenarios and tests

--- core/src/androidTest/java/cz/csas/cscore/locker/LockerUnlockMigrationPasswordServerError.java
+++ core/src/androidTest/java/cz/csas/cscore/locker/LockerUnlockMigrationPasswordServerError.java
@@ -7,13 +7,13 @@ import java.util.concurrent.TimeUnit;
 
 import cz.csas.cscore.LockerTest;
 import cz.csas.cscore.client.rest.CsCallback;
+import cz.csas.cscore.client.rest.CsRestError;
 import cz.csas.cscore.client.rest.client.Response;
 import cz.csas.cscore.error.CsSDKError;
 import cz.csas.cscore.judge.Constants;
 import cz.csas.cscore.judge.JudgeUtils;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 
 /**
  * The type Locker unlock success test.
@@ -21,16 +21,16 @@ import static junit.framework.Assert.assertTrue;
  * @author Jan Hauser <jan.hauser@applifting.cz>
  * @since 06 /12/15.
  */
-public class LockerUnlockMigration extends LockerTest {
+public class LockerUnlockMigrationPasswordServerError extends LockerTest {
 
-    private final String X_JUDGE_SESSION_HEADER_UNLOCK_MIGRATION = "core.locker.unlockMigration";
+    private final String X_JUDGE_SESSION_HEADER_UNLOCK_MIGRATION_PASSWORD_SERVER_ERROR = "core.locker.unlockMigration.password.serverError";
     private CountDownLatch mUnlockSignal;
-    private RegistrationOrUnlockResponse mUnlockResponse;
     private State mState;
+    private CsRestError mError;
 
     @Override
     public void setUp() {
-        mXJudgeSessionHeader = X_JUDGE_SESSION_HEADER_UNLOCK_MIGRATION;
+        mXJudgeSessionHeader = X_JUDGE_SESSION_HEADER_UNLOCK_MIGRATION_PASSWORD_SERVER_ERROR;
         super.setUp();
         mUnlockSignal = new CountDownLatch(1);
         LockerUtils.setLocker(mLocker, mXJudgeSessionHeader, mCryptoManager);
@@ -41,14 +41,14 @@ public class LockerUnlockMigration extends LockerTest {
                 mState = state;
             }
         });
-        JudgeUtils.setJudge(mJudgeClient, X_JUDGE_SESSION_HEADER_UNLOCK_MIGRATION, mXJudgeSessionHeader);
+        JudgeUtils.setJudge(mJudgeClient, X_JUDGE_SESSION_HEADER_UNLOCK_MIGRATION_PASSWORD_SERVER_ERROR, mXJudgeSessionHeader);
     }
 
     /**
      * Test.
      */
     @Test
-    public void testUnlockMigration() {
+    public void testUnlockMigrationPasswordServerError() {
 
         mLocker.unlockAfterMigration(new Password(LockType.FINGERPRINT, Constants.PASSWORD_TEST),
                 new PasswordHashProcess() {
@@ -65,12 +65,12 @@ public class LockerUnlockMigration extends LockerTest {
                 new CsCallback<RegistrationOrUnlockResponse>() {
                     @Override
                     public void success(RegistrationOrUnlockResponse registrationOrUnlockResponse, Response response) {
-                        mUnlockResponse = registrationOrUnlockResponse;
                         mUnlockSignal.countDown();
                     }
 
                     @Override
                     public void failure(CsSDKError error) {
+                        mError = (CsRestError) error;
                         mUnlockSignal.countDown();
                     }
                 });
@@ -82,12 +82,8 @@ public class LockerUnlockMigration extends LockerTest {
             e.printStackTrace();
         }
 
-        assertEquals(State.USER_UNLOCKED, mState);
-        assertEquals(mUnlockResponse.getAccessToken().getAccessToken(), Constants.ACCESS_TOKEN_TEST);
-        assertEquals(mUnlockResponse.getAccessToken().getAccessTokenExpiration(), Constants.ACCESS_TOKEN_EXPIRATION_TEST);
-        assertTrue(mLocker.getStatus().hasAesEncryptionKey());
-        assertTrue(mLocker.getStatus().hasOneTimePasswordKey());
-        assertEquals(mLocker.getAccessToken().getAccessToken(), mUnlockResponse.getAccessToken().getAccessToken());
-        assertEquals(mLocker.getAccessToken().getAccessTokenExpiration(), mUnlockResponse.getAccessToken().getAccessTokenExpiration());
+        assertEquals(State.USER_UNREGISTERED, mState);
+        assertEquals(mError.getKind(), CsRestError.Kind.HTTP);
+        assertEquals(mError.getResponse().getStatus(), 500);
     }
 }
